e1c3af795a17bc9a56e7e6998d3817bbf83dec10
Tobias Wich <tobias.wich@ecsec.de>
2014-02-02 20:21:09 +0100

    Add module's path in wsgi starter

--- lockzog/wsgi.py
+++ lockzog/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# add project module to system path
+path = os.path.dirname(os.path.abspath(__file__)) + "/.."
+if path not in sys.path:
+    sys.path.append(path)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lockzog.settings")
 
 from django.core.wsgi import get_wsgi_application
